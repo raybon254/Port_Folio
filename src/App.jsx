c9edf8c359342b81b91ebdb4f2d5465630745aef
Brian <musyimib449@gmail.com>
2025-08-26 04:06:53 +0300

    project link inluded

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <About />
       < Skills />
       <Projects />
-      <Contact />
+      {/* <Contact /> */}
       <Footer />
     </>
   );
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,9 +2,10 @@ import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 function Projects() {
   const projects = [
-    { title: "shopping store", desc: "A sleek and user-friendly online store showcasing a variety of phone products.", link: "#" },
-    { title: "fleet fusion", desc: "A comprehensive fleet and passenger management system for bus companies.", link: "#" },
-    { title: "Project 3", desc: "Mobile-first UI design", link: "#" },
+    { title: "shopping store", desc: "A sleek and user-friendly online store showcasing a variety of phone products.", link: "https://shopping-store-szp7.onrender.com/", demo:"https://shopping-store-szp7.onrender.com/" },
+    { title: "fleet fusion", desc: "A comprehensive fleet and passenger management system for bus companies.", link: "https://github.com/raybon254/FLEET_FUSION.git", demo:"https://github.com/raybon254/FLEET_FUSION.git" },
+    { title: "marks calculator", desc: "A web-based application that allows teachers, students, or parents to easily record and manage student marks across different subjects.", link: "https://github.com/raybon254/Marks_calculator.git", demo:"https://marks-calculator-yyz5-asd1wpueh-rays-projects-be2f0007.vercel.app/" },
+    { title: "expense tracker", desc: "A simple and intuitive web application to track your daily expenses. Easily add, view, and manage your transactions to stay on top of your finances.", link: "https://github.com/raybon254/expense-tracker.git", demo:"https://expense-tracker-2-ms4m.onrender.com/" },
   ];
 
   return (
@@ -18,8 +19,8 @@ function Projects() {
                 <Card.Body>
                   <Card.Title className="text-uppercase fw-bold">{project.title}</Card.Title>
                   <Card.Text>{project.desc}</Card.Text>
-                  <Button variant="primary" href="https://shopping-store-szp7.onrender.com/" className="m-2">View</Button>
-                  <Button variant="primary" href="https://shopping-store-szp7.onrender.com/" className="m-2">Git</Button>
+                  <Button variant="primary" href={project.link} className="m-2">View</Button>
+                  <Button variant="primary" href={project.demo} className="m-2">Demo</Button>
                 </Card.Body>
               </Card>
             </Col>
